1ee9877bd039a356345c214bed0e93d12cf9f06c
iLoveCepelinai <71884545+iLoveCepelinai@users.noreply.github.com>
2020-12-18 12:59:11 +0200

    Add files via upload

--- amsiejus_v1.0/amsiejus_v1.0.cpp
+++ amsiejus_v1.0/amsiejus_v1.0.cpp
@@ -10,7 +10,9 @@ int main()
 {
     string pasirinkimas;
 
-    list <studentas> grupe, dundukai;   //<----- cia butina pakeisti i vector vietoje list jei norite pabandyti daryti su vector
+    list <studentas> grupe, dundukai, sukciukai;
+
+    vector <studentas> grupeV, dundukaiV, sukciukaiV;
 
     do {
         cout << "Iveskite pagal ka skaiciuosite vidurki (vid/med)" << endl;
@@ -34,6 +36,68 @@ int main()
 
     failuKurimas(5, 1000);
 
+    cout << "---------vector pirmas budas---------" << endl;
+
+    skaitymasVec(1000, grupeV, pasirinkimas);//
+
+    skirstymas1Vec(grupeV, dundukaiV, sukciukaiV);//
+
+    surasymasVec(dundukaiV, sukciukaiV, "dundukai1000.txt", "sukciukai1000.txt");//
+    sukciukaiV.clear();
+    dundukaiV.clear();//
+
+    cout << endl;
+
+    cout << "---------vector antras budas---------" << endl;
+
+    skaitymasVec(1000, grupeV, pasirinkimas);
+
+    skirstymas2Vec(grupeV, dundukaiV);
+
+    surasymasVec(dundukaiV, grupeV, "dundukai1000.txt", "sukciukai1000.txt");
+    grupeV.clear();
+    dundukaiV.clear();
+
+    cout << endl;
+
+    cout << "--------vector mano budas------------" << endl;
+
+    skaitymasVec(1000, grupeV, pasirinkimas);//
+
+    skirstymasManoVector(grupeV, sukciukaiV);//
+
+    surasymasVec(grupeV, sukciukaiV, "dundukai1000.txt", "sukciukai1000.txt");//
+    grupe.clear();
+    sukciukai.clear();//
+
+    cout << endl;
+
+    cout << "--------list pirmas budas------------" << endl;
+
+    skaitymasList(1000, grupe, pasirinkimas);//
+
+    skirstymas1List(grupe, dundukai, sukciukai);//
+
+    surasymasList(dundukai, sukciukai, "dundukai1000.txt", "sukciukai1000.txt");//
+    sukciukai.clear();
+    dundukai.clear();//
+
+    cout << endl;
+
+    cout << "--------list antras budas------------" << endl;
+
+    skaitymasList(1000, grupe, pasirinkimas);
+
+    skirstymas2List(grupe, dundukai);
+
+    surasymasList(dundukai, grupe, "dundukai1000.txt", "sukciukai1000.txt");
+    grupe.clear();
+    dundukai.clear();
+
+    cout << endl;
+
+    cout << "----------list mano budas------------" << endl;
+
     skaitymasList(1000, grupe, pasirinkimas);//
 
     skirstymasManoList(grupe, dundukai);//
@@ -51,6 +115,68 @@ int main()
     cout << "-------------10 000-------------" << endl;
 
     failuKurimas(5, 10000);
+
+    cout << "---------vector pirmas budas---------" << endl;
+
+    skaitymasVec(10000, grupeV, pasirinkimas);//
+
+    skirstymas1Vec(grupeV, dundukaiV, sukciukaiV);//
+
+    surasymasVec(dundukaiV, sukciukaiV, "dundukai10000.txt", "sukciukai10000.txt");//
+    sukciukaiV.clear();
+    dundukaiV.clear();//
+
+    cout << endl;
+
+    cout << "---------vector antras budas---------" << endl;
+
+    skaitymasVec(10000, grupeV, pasirinkimas);
+
+    skirstymas2Vec(grupeV, dundukaiV);
+
+    surasymasVec(dundukaiV, grupeV, "dundukai10000.txt", "sukciukai10000.txt");
+    grupeV.clear();
+    dundukaiV.clear();
+
+    cout << endl;
+
+    cout << "--------vector mano budas------------" << endl;
+
+    skaitymasVec(10000, grupeV, pasirinkimas);
+
+    skirstymasManoVector(grupeV, sukciukaiV);
+
+    surasymasVec(grupeV, sukciukaiV, "dundukai10000.txt", "sukciukai10000.txt");
+    grupe.clear();
+    sukciukai.clear();
+
+    cout << endl;
+
+    cout << "--------list pirmas budas------------" << endl;
+
+    skaitymasList(10000, grupe, pasirinkimas);//
+
+    skirstymas1List(grupe, dundukai, sukciukai);//
+
+    surasymasList(dundukai, sukciukai, "dundukai10000.txt", "sukciukai10000.txt");//
+    sukciukai.clear();
+    dundukai.clear();//
+
+    cout << endl;
+
+    cout << "--------list antras budas------------" << endl;
+
+    skaitymasList(10000, grupe, pasirinkimas);
+
+    skirstymas2List(grupe, dundukai);
+
+    surasymasList(dundukai, grupe, "dundukai10000.txt", "sukciukai10000.txt");
+    grupe.clear();
+    dundukai.clear();
+
+    cout << endl;
+
+    cout << "----------list mano budas------------" << endl;
 
     skaitymasList(10000, grupe, pasirinkimas);
 
@@ -70,6 +196,68 @@ int main()
 
     failuKurimas(5, 100000);
 
+    cout << "---------vector pirmas budas---------" << endl;
+
+    skaitymasVec(100000, grupeV, pasirinkimas);//
+
+    skirstymas1Vec(grupeV, dundukaiV, sukciukaiV);//
+
+    surasymasVec(dundukaiV, sukciukaiV, "dundukai100000.txt", "sukciukai100000.txt");//
+    sukciukaiV.clear();
+    dundukaiV.clear();//
+
+    cout << endl;
+
+    cout << "---------vector antras budas---------" << endl;
+
+    skaitymasVec(100000, grupeV, pasirinkimas);
+
+    skirstymas2Vec(grupeV, dundukaiV);
+
+    surasymasVec(dundukaiV, grupeV, "dundukai100000.txt", "sukciukai100000.txt");
+    grupeV.clear();
+    dundukaiV.clear();
+
+    cout << endl;
+
+    cout << "--------vector mano budas------------" << endl;
+
+    skaitymasVec(100000, grupeV, pasirinkimas);
+
+    skirstymasManoVector(grupeV, sukciukaiV);
+
+    surasymasVec(grupeV, sukciukaiV, "dundukai100000.txt", "sukciukai100000.txt");
+    grupe.clear();
+    sukciukai.clear();
+
+    cout << endl;
+
+    cout << "--------list pirmas budas------------" << endl;
+
+    skaitymasList(100000, grupe, pasirinkimas);//
+
+    skirstymas1List(grupe, dundukai, sukciukai);//
+
+    surasymasList(dundukai, sukciukai, "dundukai100000.txt", "sukciukai100000.txt");//
+    sukciukai.clear();
+    dundukai.clear();//
+
+    cout << endl;
+
+    cout << "--------list antras budas------------" << endl;
+
+    skaitymasList(100000, grupe, pasirinkimas);
+
+    skirstymas2List(grupe, dundukai);
+
+    surasymasList(dundukai, grupe, "dundukai100000.txt", "sukciukai100000.txt");
+    grupe.clear();
+    dundukai.clear();
+
+    cout << endl;
+
+    cout << "----------list mano budas------------" << endl;
+
     skaitymasList(100000, grupe, pasirinkimas);
 
     skirstymasManoList(grupe, dundukai);
@@ -88,6 +276,68 @@ int main()
 
     failuKurimas(5, 1000000);
 
+    cout << "---------vector pirmas budas---------" << endl;
+
+    skaitymasVec(1000000, grupeV, pasirinkimas);//
+
+    skirstymas1Vec(grupeV, dundukaiV, sukciukaiV);//
+
+    surasymasVec(dundukaiV, sukciukaiV, "dundukai1000000.txt", "sukciukai1000000.txt");//
+    sukciukaiV.clear();
+    dundukaiV.clear();//
+
+    cout << endl;
+
+    cout << "---------vector antras budas---------" << endl;
+
+    skaitymasVec(1000000, grupeV, pasirinkimas);
+
+    skirstymas2Vec(grupeV, dundukaiV);
+
+    surasymasVec(dundukaiV, grupeV, "dundukai1000000.txt", "sukciukai1000000.txt");
+    grupeV.clear();
+    dundukaiV.clear();
+
+    cout << endl;
+
+    cout << "--------vector mano budas------------" << endl;
+
+    skaitymasVec(1000000, grupeV, pasirinkimas);
+
+    skirstymasManoVector(grupeV, sukciukaiV);
+
+    surasymasVec(grupeV, sukciukaiV, "dundukai1000000.txt", "sukciukai1000000.txt");
+    grupe.clear();
+    sukciukai.clear();
+
+    cout << endl;
+
+    cout << "--------list pirmas budas------------" << endl;
+
+    skaitymasList(1000000, grupe, pasirinkimas);//
+
+    skirstymas1List(grupe, dundukai, sukciukai);//
+
+    surasymasList(dundukai, sukciukai, "dundukai1000000.txt", "sukciukai1000000.txt");//
+    sukciukai.clear();
+    dundukai.clear();//
+
+    cout << endl;
+
+    cout << "--------list antras budas------------" << endl;
+
+    skaitymasList(1000000, grupe, pasirinkimas);
+
+    skirstymas2List(grupe, dundukai);
+
+    surasymasList(dundukai, grupe, "dundukai1000000.txt", "sukciukai1000000.txt");
+    grupe.clear();
+    dundukai.clear();
+
+    cout << endl;
+
+    cout << "----------list mano budas------------" << endl;
+
     skaitymasList(1000000, grupe, pasirinkimas);
 
     skirstymasManoList(grupe, dundukai);
@@ -104,6 +354,68 @@ int main()
     cout << "-------------10 000 000-------------" << endl;
 
     failuKurimas(5, 10000000);
+
+    cout << "---------vector pirmas budas---------" << endl;
+
+    skaitymasVec(10000000, grupeV, pasirinkimas);//
+
+    skirstymas1Vec(grupeV, dundukaiV, sukciukaiV);//
+
+    surasymasVec(dundukaiV, sukciukaiV, "dundukai10000000.txt", "sukciukai10000000.txt");//
+    sukciukaiV.clear();
+    dundukaiV.clear();//
+
+    cout << endl;
+
+    cout << "---------vector antras budas---------" << endl;
+
+    skaitymasVec(10000000, grupeV, pasirinkimas);
+
+    skirstymas2Vec(grupeV, dundukaiV);
+
+    surasymasVec(dundukaiV, grupeV, "dundukai10000000.txt", "sukciukai10000000.txt");
+    grupeV.clear();
+    dundukaiV.clear();
+
+    cout << endl;
+
+    cout << "--------vector mano budas------------" << endl;
+
+    skaitymasVec(10000000, grupeV, pasirinkimas);
+
+    skirstymasManoVector(grupeV, sukciukaiV);
+
+    surasymasVec(grupeV, sukciukaiV, "dundukai10000000.txt", "sukciukai10000000.txt");
+    grupe.clear();
+    sukciukai.clear();
+
+    cout << endl;
+
+    cout << "--------list pirmas budas------------" << endl;
+
+    skaitymasList(10000000, grupe, pasirinkimas);//
+
+    skirstymas1List(grupe, dundukai, sukciukai);//
+
+    surasymasList(dundukai, sukciukai, "dundukai10000000.txt", "sukciukai10000000.txt");//
+    sukciukai.clear();
+    dundukai.clear();//
+
+    cout << endl;
+
+    cout << "--------list antras budas------------" << endl;
+
+    skaitymasList(10000000, grupe, pasirinkimas);
+
+    skirstymas2List(grupe, dundukai);
+
+    surasymasList(dundukai, grupe, "dundukai10000000.txt", "sukciukai10000000.txt");
+    grupe.clear();
+    dundukai.clear();
+
+    cout << endl;
+
+    cout << "----------list mano budas------------" << endl;
 
     skaitymasList(10000000, grupe, pasirinkimas);//
 
